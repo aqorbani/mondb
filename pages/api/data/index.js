86bdfeb1a41d5350611bd9b385b0d275ef09df9d
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-16 18:56:25 +0330

    error handling

--- pages/api/data/index.js
+++ pages/api/data/index.js
@@ -2,7 +2,14 @@ import User from "@/models/User";
 import connectDB from "@/utils/ConnectDB";
 
 export default async function handler(req, res) {
-  await connectDB();
+  try {
+    await connectDB();
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ status: "failed", message: "Error in connecting to server..." });
+  }
   if (req.method === "POST") {
     const { name } = req.body;
     if (!name || name.lenght <= 3) {
@@ -10,15 +17,18 @@ export default async function handler(req, res) {
       return;
     }
 
-    //Save Data
-    // const user = new User({ name });
-    // await user.save();
+    try {
+      const user = await User.create({ name });
 
-    const user = await User.create({ name });
-    console.log(user);
-
-    res
-      .status(201)
-      .json({ status: "success", message: "Data created.", data: { user } });
+      res
+        .status(201)
+        .json({ status: "success", message: "Data created.", data: { user } });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        status: "failed",
+        message: "Error in connecting to server... (insert)",
+      });
+    }
   }
 }
--- utils/ConnectDB.js
+++ utils/ConnectDB.js
@@ -1,22 +1,15 @@
 import mongoose from "mongoose";
 
 export default async function connectDB() {
-  try {
-    //Connect to DB
+  //Connect to DB
 
-    if (mongoose.connections[0].readyState) return;
+  if (mongoose.connections[0].readyState) return;
 
-    mongoose.set("strictQuery", false);
-    await mongoose
-      .connect(
-        "mongodb+srv://aqorbani:oUBk4QEnooiwJk5C@cluster0.snpyoz0.mongodb.net/?retryWrites=true&w=majority",
-        {
-          serverSelectionTimeoutMS: 10000,
-        }
-      )
-      .then(() => console.log("Database connected!"))
-      .catch((err) => console.log(err));
-  } catch (error) {
-    console.log(error);
-  }
+  mongoose.set("strictQuery", false);
+  await mongoose
+    .connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 10000,
+    })
+    .then(() => console.log("Database connected!"))
+    .catch((err) => console.log(err));
 }
